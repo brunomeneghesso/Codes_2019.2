8c3269826ccdb423c372ef9249358f4faf584363
pbcarani <49569772+pbcarani@users.noreply.github.com>
2019-09-14 13:24:23 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   let db = coDesConnect('https://projetofolhaum.firebaseio.com/')
 
   db.download('/', function(data) {
-    context = data
-    coDesReplace("section-menu", context)
-    coDesReplace("section-list", context)
+
+
+    let context = data
+    coDesReplace('.section-menu', context)
+
+
+
+
+
+
   })
 })
