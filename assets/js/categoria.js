a137f62ce5389d7558d98cd49b4a6552fc6a2001
Maulem <henriquemmarti@gmail.com>
2019-09-17 11:31:14 -0300

    a

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
   let db = coDesConnect('https://projetofolhaum.firebaseio.com/')
 
   db.download('/', function(data) {
 
 
-    let context = data
+    let context = data['biblioteca'][value]
     coDesReplace('.local', context)
 
 
